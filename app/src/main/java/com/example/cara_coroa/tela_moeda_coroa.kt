0168562d743b93ae2133ee470e26f99da1962da9
Lucas Lima <38673203+lucaslimaax@users.noreply.github.com>
2020-02-17 15:06:02 -0300

    Initial commit

---
+++ app/src/main/java/com/example/cara_coroa/tela_moeda_coroa.kt
@@ -0,0 +1,12 @@
+package com.example.cara_coroa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class tela_moeda_coroa : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_moeda_coroa)
+    }
+}
