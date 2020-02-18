114fe9177a39135412d8661c09b4900f4081f79b
Lucas Lima <38673203+lucaslimaax@users.noreply.github.com>
2020-02-17 21:46:30 -0300

    Settings and functions for change imagens coins

--- app/src/main/java/com/example/cara_coroa/MainActivity.kt
+++ app/src/main/java/com/example/cara_coroa/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.cara_coroa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_jogar.setOnClickListener{
+            val intent = Intent(applicationContext, tela_moeda::class.java)
+
+            //gerar numero randomico
+            val numero = Random().nextInt(2)
+
+            //Enviar numero para a proxima activy
+
+            intent.putExtra(name:"nnumero", numero)
+
+            startActivity()
+
+
+        }
     }
 }
--- app/src/main/java/com/example/cara_coroa/tela_moeda.kt
+++ app/src/main/java/com/example/cara_coroa/tela_moeda.kt
@@ -2,11 +2,28 @@ package com.example.cara_coroa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_tela_moeda.*
 
 class tela_moeda : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_moeda)
+
+        val dados: Bundle = intent.extras
+        val numero = dados.getInt("numero")
+
+        if(numero == 0){
+            img_cara.setImageResource(R.drawable.moeda_cara)
+
+        }
+        else{
+            img_cara.setImageResource(R.drawable.moeda_cara)
+
+        }
+
+        button_voltar.setOnClickListener{
+            finish()
+        }
     }
 }
